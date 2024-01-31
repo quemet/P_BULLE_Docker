e8b07574a6d74e8cf45094f9a3f6491bf8a84d3a
quemet <quentinmet7@gmail.com>
2024-01-31 17:21:29 +0100

    Ajout du code

---
+++ P_Bulle_Docker/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const createdBoard = () => {
+    for (let i = 0; i < 10; i++) {
+        let div = document.createElement("div");
+        div.id = "gameBoard";
+        document.getElementsByClassName("text-center").
+    }
+}
+
+createdBoard();
