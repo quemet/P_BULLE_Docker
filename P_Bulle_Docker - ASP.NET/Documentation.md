69cedf421a1be76a3cdf90f5a19a14f4569bb950
Quentin <quentinmet7@gmail.com>
2024-03-26 13:40:00 +0100

    Commencer la documentation pour le projet

---
+++ P_Bulle_Docker - ASP.NET/Documentation.md
@@ -0,0 +1,3 @@
+# P_Bulle_Dev_Docker
+
+## Creation du containeur avec DevEnvironments
---
+++ P_Bulle_Docker - ASP.NET/env.sh
@@ -0,0 +1,17 @@
+sudo -i
+
+apt-get upgrade
+
+apt-get install sudo
+
+sudo apt-get update
+
+sudo apt-get install -y wget
+
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.1/powershell_7.4.1-1.deb_amd64.deb
+
+sudo dpkg -i powershell_7.4.1-1.deb_amd64.deb
+
+sudo apt-get install -f
+
+pwsh
