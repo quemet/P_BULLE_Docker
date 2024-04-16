3563475db3d0c0c03c5e17e13066b6141ea571c2
Quentin <quentinmet7@gmail.com>
2024-04-16 15:41:09 +0200

    Fin du cours 3

--- P_Bulle_Docker - ASP.NET/build.sh
+++
@@ -1,3 +0,0 @@
-# docker build -t test .
-# docker run -p 8080:80 test
-# google-chrome http://localhost:8080
--- P_Bulle_Docker - ASP.NET/env.sh
+++ P_Bulle_Docker - ASP.NET/env.sh
@@ -1,13 +1,1 @@
 docker-compose up
-# apt-get upgrade
-# apt-get install sudo
-# sudo apt update
-# sudo apt install -y wget apt-transport-https software-properties-commo
-# wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-# sudo dpkg -i packages-microsoft-prod.deb
-# sudo apt update
-# sudo apt install -y aspnetcore-runtime-6.0
-# sudo apt update
-# sudo apt install -y dotnet-sdk-6.0
-# cd ../com.docker.devenvironments.code/bin/Debug/net6.0
-# dotnet P_Bulle_Docker.dll
