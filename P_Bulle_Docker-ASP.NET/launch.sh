3c649a383e0137e75531979fccc272e200dd48bc
quemet <129742514+quemet@users.noreply.github.com>
2024-05-21 15:32:53 +0200

    Update launch.sh

--- P_Bulle_Docker-ASP.NET/launch.sh
+++ P_Bulle_Docker-ASP.NET/launch.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 dotnet dev-certs https
 
+# Informe et installe sudo
 echo ""
 echo "Installtion de sudo"
 apt-get update
 apt-get install sudo
 echo ""
 
+# Informe et installe xdg
 echo ""
 echo "Installer les dépendances nécessaires à xdg"
 sudo apt-get update
 sudo apt-get install xdg-utils
 echo ""
 
+# Lance une page web avec l'url suivante
 xdg-open 'https://localhost:7218'
 
+# lance l'application .NET
 dotnet run
