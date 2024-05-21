78683db3db635a3263679ce74626bd1c2901eef2
quemet <129742514+quemet@users.noreply.github.com>
2024-05-21 15:37:05 +0200

    Update build.sh

--- P_Bulle_Docker-ASP.NET/build.sh
+++ P_Bulle_Docker-ASP.NET/build.sh
@@ -1,50 +1,66 @@
 #!/bin/bash
+# Défini un tableau avec les contianers
 containers=('application' 'mysql' 'test')
+
+# Défini un tableau avec les images
 images=('p_bulle_docker-aspnet-db' 'p_bulle_docker-aspnet-test' 'p_bulle_docker-aspnet-webapp')
 
+# Informet l'utilisateur du début du script
 echo ""
 echo "Supprimer les conteneurs..."
 echo ""
 
+# Défini les variables pour un meilleur interaction utilisateurs
 count=1
 total_step=6
 
+# Boucle sur le tableau des containers
 for container in "${containers[@]}"
 do
+    # Informe, stoppe et supprimme les containers dans le tableau
     echo ""
     echo "[Step $count/$total_step] En train de supprimer le conteneur $container..."
     sudo docker stop $container
     sudo docker rm $container
     echo "Conteneur $container a été supprimé"
     echo ""
+    # Incrémente la variable count pour avoir un suivi
     ((count++))
 done
 
+# Boucle sur le tableau des images
 for image in "${images[@]}"
 do
+    # Informe et supprimme les images dans le tableau
     echo ""
     echo "[Step $count/$total_step] En train de supprimer l'image $image..."
     sudo docker rmi $image
     echo "Image $image a été supprimée"
     echo ""
+    # Incrémente la variable count pour avoir un suivi
     ((count++))
 done
 
+# Informe l'utilisateur que toute à réussi
 echo ""
 echo "Tous les conteneurs et images ont été supprimés."
 echo ""
 
+# Informe l'utilisateur du démarrage des services
 echo ""
 echo "Démarrage des services avec docker-compose..."
 echo ""
 
+# Informe et installe la librairie xdg
 echo ""
-echo "Installer les dépendances nécessaires à xpg"
+echo "Installer les dépendances nécessaires à xdg"
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install xpg-utils
+sudo apt-get install xdg-utils
 echo ""
 
+# Lance la création des services
 sudo docker-compose up -d
 
+# Ouvre une page web à l'url suivante.
 xdg-open 'http://localhost:8080'
